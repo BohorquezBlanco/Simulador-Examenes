905c3821acaf07c0344ec19c35ca5a9ab06e20ad
Genevive_xnp <165971003+GeneviveAllReady@users.noreply.github.com>
2024-04-16 11:02:52 -0400

    Mas ordenado

--- app/Views/user/plantilla/footer.php
+++ app/Views/user/plantilla/footer.php
@@ -1,5 +1,5 @@
-<footer id="footer" class="bg-dark text-light text-center p-3 fixed-bottom ">
-    <div class="container ">
+<footer id="footer">
+    <div class="container">
     <ul class="soc-media-ul">
         <li>
         <a href="https://twitter.com/AlexDevero" class="fa fa-twitter" target="_blank"></a>
@@ -22,4 +22,9 @@
         </li>
     </ul>
     </div>
-</footer>
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
